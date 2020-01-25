814c8b42f033c0936571ee6074e77b9194a11218
Karol Sobański <sobanskikarol91@gmail.com>
2020-01-25 13:20:06 +0100

    Add: Extinguisher Basic mechanic

---
+++ Task/Assets/Scripts/FireableObject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class FireableObject : MonoBehaviour
+{
+    [SerializeField] float maxHealth = 10000;
+    [SerializeField] ParticleSystem particles;
+
+    private float startFireLength;
+    private float currentHealth;
+    private float increaseHealthWithTime = 0.5f;
+
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+        startFireLength = particles.startLifetime;
+    }
+
+    private void Update()
+    {
+        if (particles.isPlaying)
+            IncreaseHealth(increaseHealthWithTime);
+    }
+
+    public void IncreaseHealth(float amount)
+    {
+        currentHealth += amount;
+        UpdateParticles();
+    }
+
+    public void DecreaseHealth(float amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+            particles.Stop();
+
+        UpdateParticles();
+    }
+
+    private void UpdateParticles()
+    {
+        particles.startLifetime = currentHealth / maxHealth * startFireLength;
+        Debug.Log(currentHealth + " " + particles.startLifetime);
+    }
+}
---
+++ Task/Assets/Scripts/Water.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Water : MonoBehaviour
+{
+    [SerializeField] float power = 2f;
+    [SerializeField] float lifeTime = 2f;
+
+    ParticleSystem particles;
+
+    private void Awake()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        lifeTime-= Time.deltaTime;
+
+        if (lifeTime <= 0)
+            StopExtinguisher();
+    }
+
+    private void StopExtinguisher()
+    {
+        particles.Stop();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        FireableObject fireable = other.GetComponent<FireableObject>();
+
+        if (fireable)
+            fireable.DecreaseHealth(power);
+    }
+}
